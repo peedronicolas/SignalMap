58002a34a6c50acb67043bbe15c88408a3e17053
Pedro nicolas <pedronicolas62@gmail.com>
2023-03-14 18:07:51 +0100

    Primera version de grafico horizontal de maximos por etapas.

--- app/src/main/java/es/um/inf/signalmap/AnalysisActivity.java
+++ app/src/main/java/es/um/inf/signalmap/AnalysisActivity.java
@@ -9,16 +9,19 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class AnalysisActivity extends AppCompatActivity {
@@ -42,14 +45,68 @@ public class AnalysisActivity extends AppCompatActivity {
         textViewFileName.setText(new File(filePath).getName());
 
         mostrarGraficoBarrasSenal();
+        mostrarGraficoBarrasHorizontal();
 
         // Establecemos el contenido del fichero
         TextView textViewFileContent = findViewById(R.id.textViewFileContent);
         textViewFileContent.setText(new GsonBuilder().setPrettyPrinting().create().toJson(networkData));
     }
 
+    public void mostrarGraficoBarrasHorizontal() {
+        HorizontalBarChart horizontalBarChart = findViewById(R.id.horizontalBarChart);
+
+        int numEtapas = (int) Arrays.stream(networkData).mapToDouble(NetworkData::getEtapa).max().orElse(0.0);
+
+        // Personalizar eje X
+        XAxis xAxis = horizontalBarChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setDrawAxisLine(true);
+        xAxis.setGranularity(1f); // Establecer el espacio entre cada barra
+        xAxis.setLabelCount(numEtapas); // Establecer el número de etiquetas
+        xAxis.setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getFormattedValue(float value) {
+                return getResources().getString(R.string.etapa) + " " + (int) value;
+            }
+        });
+
+        // Personalizar eje Y
+        YAxis leftAxis = horizontalBarChart.getAxisLeft();
+        leftAxis.setDrawAxisLine(false);
+        leftAxis.setDrawGridLines(false);
+        leftAxis.setDrawLabels(true);
+
+        // Añadimos las barras con los datos
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        for (int i = 1; i <= numEtapas; i++) {
+            final int etapa = i;
+            double maxEtapa = Arrays.stream(networkData).filter(ND -> ND.getEtapa() == etapa).mapToDouble(NetworkData::getMaxSignal).max().orElse(0.0);
+            entries.add(new BarEntry(i, (float) maxEtapa));
+        }
+
+        // Crear conjunto de datos y personalizar colores
+        BarDataSet dataSet = new BarDataSet(entries, "Signal Values");
+        dataSet.setColor(Color.parseColor("#99C9FF"));
+
+        // Crear objeto BarData con el conjunto de datos y etiquetas
+        BarData barData = new BarData(dataSet);
+
+        // Personaliza y muestra el grafico
+        horizontalBarChart.setData(barData);
+        horizontalBarChart.getLegend().setEnabled(false); // Desactivamos la leyenda
+        horizontalBarChart.setDescription(null);
+        horizontalBarChart.setDrawGridBackground(false);
+        horizontalBarChart.setTouchEnabled(false);
+        horizontalBarChart.setDragEnabled(false);
+        horizontalBarChart.setScaleEnabled(false);
+        horizontalBarChart.setPinchZoom(false);
+        horizontalBarChart.setDrawValueAboveBar(true);
+        horizontalBarChart.invalidate(); // Actualizar grafico
+    }
+
     public void mostrarGraficoBarrasSenal() {
-        BarChart barChart = findViewById(R.id.chart1);
+        BarChart barChart = findViewById(R.id.barChartSignal);
 
         // Configuración del eje x
         XAxis xAxis = barChart.getXAxis();
@@ -70,7 +127,7 @@ public class AnalysisActivity extends AppCompatActivity {
         for (int i = 0; i < networkData.length; i++)
             entries.add(new BarEntry(i, (float) networkData[i].getMaxSignal()));
 
-        BarDataSet dataSet = new BarDataSet(entries, "Valores");
+        BarDataSet dataSet = new BarDataSet(entries, "Signal Values");
 
         // Configuración de los colores de las barras segun el valor
         List<Integer> colors = new ArrayList<>();
@@ -86,6 +143,7 @@ public class AnalysisActivity extends AppCompatActivity {
         }
         dataSet.setColors(colors);
 
+        // Personaliza y muestra el grafico
         BarData data = new BarData(dataSet);
         barChart.setData(data);
         barChart.getLegend().setEnabled(false); // Desactivamos la leyenda
